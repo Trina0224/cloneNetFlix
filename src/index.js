946724e35e52e036367453f4662c9ef78c9384d6
Trina0224 <trina.shih@gmail.com>
2021-02-02 12:20:41 -0800

    20 connecting app to firebase

--- src/index.js
+++ src/index.js
@@ -4,24 +4,35 @@ import { GlobalStyles } from './global-styles';
 import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 
+//const clientID = process.env.REACT_APP_UNSPLASH4GOD_API_KEY;
+
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
 }
 
-const firebase = window.firebase.initializeApp(config);
+// const firebase = window.firebase.initializeApp(config);
+//
+// ReactDOM.render(
+//     <>
+//         <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+//             <GlobalStyles />
+//             <App />
+//         </FirebaseContext.Provider>
+//     </>,
+//     document.getElementById('root')
+// );
+
 
 ReactDOM.render(
-    <>
-        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
-            <GlobalStyles />
-            <App />
-        </FirebaseContext.Provider>
-    </>, 
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>,
     document.getElementById('root')
 );
